3713178932d5b4a8b026c3165f08c34d3182a54c
ahmad-ben <ahmadbenchakhti@gmail.com>
2024-10-23 23:38:47 +0100

    Add Section 29 -Dynamic Programming-

---
+++ Colt Steele/Section 29 -Dynamic Programming-/Scripts/V9 -Tabulation A Bottom Up Approach-/my-try-fibonacci-sequence-with-tabulation.js
@@ -0,0 +1,18 @@
+const fib = (degree) => {
+  if(degree < 3) return degree;
+
+  let prePreviousValue = 1, previousValue = 1;
+  let currentValue;
+  for (let i = 2; i < degree; i++) {
+    currentValue = previousValue + prePreviousValue; 
+    prePreviousValue = previousValue; 
+    previousValue = currentValue; 
+  }
+
+  return currentValue;
+};
+
+console.log(fib(2));
+console.log(fib(1));
+console.log(fib(5));
+console.log(fib(10));
